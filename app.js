884feb275f2d7901ab6df4d21484d010328c1ba9
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-06-18 09:26:01 +0300

    Handle route with different params and querys

--- app.js
+++ app.js
@@ -12,6 +12,21 @@ app.get('/api/customers', (req, res) => {
   res.send(['Ahmed', 'Ali', 'Mark', 'Mona']);
 });
 
+// Handle a route with one param
+app.get('/api/customers/:id', (req, res) => {
+  res.send(req.params.id);
+});
+
+// Handle a route with multiple params
+app.get('/api/customers/:id/:date', (req, res) => {
+  res.send(req.params);
+});
+
+// Handle a route with query params which are optional
+app.get('/api/customers/:id/:date/:name', (req, res) => {
+  res.send(req.query);
+});
+
 // app.post()
 
 // app.put()
